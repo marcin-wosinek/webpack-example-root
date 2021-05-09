aa04b3670efbba0c01d6c5f6bb88088341cb63a9
Marcin Wosinek <marcin@wosinek.eu>
2021-05-09 16:18:49 +0100

    add solution for lazy-load example

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 const element = document.getElementById("core");
 
 element.innerHTML = "✓ Core module is loaded correclty";
+
+const pdfButton = document.getElementById("pdf-button");
+
+import(/* webpackChunkName: "pdf-lib" */ "pdf-lib").then(({ PDFDocument }) => {
+  document.getElementById("pdf-lib").innerHTML =
+    "✓ pdf-lib is loaded correclty";
+
+  pdfButton.addEventListener("click", async () => {
+    const pdfDoc = await PDFDocument.create();
+    const page = pdfDoc.addPage([350, 400]);
+    page.moveTo(110, 200);
+    page.drawText("Hello World!");
+    const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+    document.getElementById("pdf").src = pdfDataUri;
+  });
+});
